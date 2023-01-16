eef967a32f3c037b60dea27e8c5682da23c223e9
X-Ella-X <e.felsinger@web.de>
2023-01-16 15:31:46 +0100

    add useEffect useParams

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { registerLocale, setDefaultLocale } from "react-datepicker";
@@ -10,31 +10,23 @@ import { FaSearch, FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export function Homepage() {
+  const [input, setInput] = useState("");
   const navigate = useNavigate();
-  const [search, setSearch] = useState({ word: "" });
-  const [data, setData] = useState();
-  const [startDate, setStartDate] = useState(new Date(), { locale: "de" });
-  const changeHandler = (e) => {
-    setSearch((prev) => (prev = { ...prev, [e.target.id]: e.target.value }));
-  };
-  const fetchData = () => {
-    fetch(`mongodb/${search.search}`)
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  };
   const submitHandler = (e) => {
     e.preventDefault();
-    fetchData();
+    navigate("/results/" + input);
   };
+  const [search, setSearched] = useState([]);
+  let params = useParams();
+  const getSearched = async () => {
+    const data = await fetch(`www.mongodb.com/events`);
+    const recipes = await data.json();
+    setSearched(recipes);
+  };
+  useEffect(() => {
+    getSearched(params.searched);
+  }, [params.searched]);
 
-  const addItem = async (param) => {
-    const data = await fetch(`mongodb/user/${param}`, {
-      method: `POST`,
-    });
-    const detailData = await data.json();
-    fetchData();
-    alert("There is no such event");
-  };
   return (
     <div>
       <h1>Suche nach Events in deiner Nähe</h1>
@@ -47,7 +39,8 @@ export function Homepage() {
           className="city"
           id="city"
           placeholder="Berlin, Hamburg, München, ..."
-          onChange={changeHandler}
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
         />
         <div className="dropdown">
           <button
@@ -60,17 +53,32 @@ export function Homepage() {
           </button>
           <ul className="dropdown-menu">
             <li>
-              <a className="dropdown-item" onChange={changeHandler} href="#">
+              <a
+                className="dropdown-item"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                href="#"
+              >
                 5 km
               </a>
             </li>
             <li>
-              <a className="dropdown-item" onChange={changeHandler} href="#">
+              <a
+                className="dropdown-item"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                href="#"
+              >
                 10 km
               </a>
             </li>
             <li>
-              <a className="dropdown-item" onChange={changeHandler} href="#">
+              <a
+                className="dropdown-item"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                href="#"
+              >
                 20 km
               </a>
             </li>
@@ -103,30 +111,35 @@ export function Homepage() {
             </th>
           </tr>
         </thead>
-        <tbody className="table-group-divider">
-          <tr>
-            <th scope="row">
-              <a href={`/event-details`}>Event 1</a>
-            </th>
-            <td>berlin</td>
-            <td>heute Abend</td>
-            <td>
-              <a href="">
-                <FaHeart />
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Event 2</th>
-            <td>stuttgart</td>
-            <td>morgen Abend</td>
-            <td>
-              <a href="">
-                <FaHeart />
-              </a>
-            </td>
-          </tr>
-        </tbody>
+        {search.events &&
+          search.events.map((item) => {
+            return (
+              <tbody className="table-group-divider">
+                <tr>
+                  <th scope="row">
+                    <a href={`/event-details`}>Event 1</a>
+                  </th>
+                  <td>berlin</td>
+                  <td>heute Abend</td>
+                  <td>
+                    <a href="">
+                      <FaHeart />
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">Event 2</th>
+                  <td>stuttgart</td>
+                  <td>morgen Abend</td>
+                  <td>
+                    <a href="">
+                      <FaHeart />
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
       </table>
     </div>
   );
