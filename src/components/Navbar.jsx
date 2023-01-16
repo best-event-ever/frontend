ab07f642dec8ddd889a55348beb541a52a0bb82c
Burak5796 <burak.d5796@gmail.com>
2023-01-16 10:59:15 +0100

    f

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ export function Navbar() {
     <div>
       <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+          <span class="navbar-brand">
             Navbar
-          </a>
+          </span>
           <button
             class="navbar-toggler"
             type="button"
@@ -24,13 +24,13 @@ export function Navbar() {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+                <a class="nav-link active" aria-current="page" href="http://localhost:5173">
                   Home
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Link
+                <a class="nav-link" href="http://localhost:5173/login">
+                  Login
                 </a>
               </li>
               <li class="nav-item dropdown">
