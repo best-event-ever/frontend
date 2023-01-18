877bfeb0734d9277e1565d74469ee005b8a235bc
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-18 09:39:10 +0100

    result text integriert

--- src/pages/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { FaHeart } from "react-icons/fa";
-
-export function SearchResultsPage() {
+import { Link } from "react-router-dom";
+export function SearchResultsPage({ search, setSearched }) {
   return (
     <div>
       <h1>SearchResultsPage</h1>
+      <input value={search} onChange={(e) => setSearched(e.target.value)} />
+      <Link to="/all-searchedevents">
+        <button
+          onClick={(e) => {
+            setSearched(search);
+          }}
+        >
+          Search
+        </button>
+      </Link>
     </div>
   );
 }
