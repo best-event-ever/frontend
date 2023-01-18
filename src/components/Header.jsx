7319e924f7df596cd28fad4947673b30f568299a
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-18 12:46:37 +0100

    image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import eventImg from "../img/jc-gellidon-XmYSlYrupL8-unsplash.jpg";
+import headerImg from "../img/header_event_app.jpg";
 
 export function Header() {
   return (
     <div className="h-20">
-      <img src={eventImg} alt="" className="w-50 h-20" />
+      <img src={headerImg} alt="" className="w-50 h-20" />
     </div>
   );
 }
